1197ae4ae59de0a15cda51bd93452b5741e15d96
AHEDeveloper <esmailiamirhosein45@gmail.com>
2026-08-20 16:51:18 +0330

    feat: add hotel reservation models and database schema

---
+++ database/migrations/2026_08_20_130736_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('amount');
+            $table->enum('status',['pending','completed','cancelled','confirmed']);
+            $table->string('transaction_id')->nullable()->unique();
+            $table->timestamp('paid_at')->nullable();
+            $table->foreignId('reservation_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
