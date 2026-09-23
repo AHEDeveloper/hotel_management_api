48cff5c39277a09222d8d143ff3d8f359efc83c5
AHEDeveloper <esmailiamirhosein45@gmail.com>
2026-09-23 12:07:03 +0330

    feat(client-auth): add register login logout with token authentication

---
+++ app/Http/Controllers/Client/V1/AuthControllerClient.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Client\V1;
+
+use App\Classes\ApiResponseClass;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthControllerClient extends Controller
+{
+    public function register(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'name' => 'required|string|min:3|max:100',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string',
+        ], [
+            'name.required' => 'وارد کردن نام الزامی است.',
+            'name.string' => 'نام باید به صورت متن باشد.',
+            'name.min' => 'نام باید حداقل ۳ کاراکتر باشد.',
+            'name.max' => 'نام نمی‌تواند بیشتر از ۱۰۰ کاراکتر باشد.',
+
+            'email.required' => 'وارد کردن ایمیل الزامی است.',
+            'email.email' => 'لطفاً یک ایمیل معتبر وارد کنید.',
+            'email.unique' => 'این ایمیل قبلاً ثبت شده است.',
+
+            'password.required' => 'وارد کردن رمز عبور الزامی است.',
+            'password.string' => 'رمز عبور باید به صورت متن باشد.',
+        ]);
+        if ($validation->fails())
+        {
+            return ApiResponseClass::errorResponse('validation fails',$validation->errors(),422);
+        }
+        $user = User::query()->create([
+           'name' => $request->name,
+           'email' => $request->email,
+           'password' => bcrypt($request->password),
+        ]);
+        $token = $user->createToken('client-token')->plainTextToken;
+        $user['token'] = $token;
+        return ApiResponseClass::apiResponse(true,'user created successfully',$user,201);
+
+    }
+
+    public function login(Request $request)
+    {
+        $validation = Validator::make($request->all(),[
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string',
+        ],[
+            'email.email' => 'لطفاً یک ایمیل معتبر وارد کنید.',
+            'email.unique' => 'این ایمیل قبلاً ثبت شده است.',
+
+            'password.string' => 'رمز عبور باید به صورت متن باشد.',
+        ]);
+
+        if($validation->fails()){
+            return ApiResponseClass::errorResponse('validation fail',$validation->errors(),422);
+        }
+        $user = User::query()->where('email',$request->email)->first();
+        $token = $user->createToken('client-token')->plainTextToken;
+        $user['token'] = $token;
+        return ApiResponseClass::apiResponse(true,'login user',$user,200);
+    }
+
+    public function logout()
+    {
+        auth()->user()->currentAccessToken()->delete();
+        return ApiResponseClass::apiResponse(true,'user is logout',null,200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Client\V1\AuthControllerClient;
 use App\Http\Controllers\Client\V1\HomePage as HomePage;
 use App\Http\Controllers\Client\V1\RoomControllerClient as RoomPage;
 use Illuminate\Support\Facades\Route;
@@ -9,12 +10,17 @@ Route::prefix('v1/admin')->group(function () {
 });
 
 Route::prefix('v1')->group(function (){
-    Route::get('/hotel',[HomePage::class,'index']);
-    Route::get('/rooms',[RoomPage::class,'index']);
-    Route::get('/rooms/{room}',[RoomPage::class,'show']);
+    Route::post('/register', [AuthControllerClient::class,'register']);
+    Route::post('/login', [AuthControllerClient::class,'login']);
+
+    Route::middleware('auth:sanctum')->group(function (){
+        Route::post('/logout', [AuthControllerClient::class,'logout']);
+        Route::get('/hotel',[HomePage::class,'index']);
+        Route::get('/rooms',[RoomPage::class,'index']);
+        Route::get('/rooms/{room}',[RoomPage::class,'show']);
+
+    });
+
+
 });
-
-
-
-
 
